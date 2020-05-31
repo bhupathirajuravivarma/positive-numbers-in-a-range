605f7e46a2e450d3a4120b280d353eac2f32b5ad
bhupathirajuravivarma <64257932+bhupathirajuravivarma@users.noreply.github.com>
2020-05-31 19:50:10 +0530

    positive no in lists

---
+++ positivenoinrange.py
@@ -0,0 +1,12 @@
+
+#positive numbers in lists
+list1 = [6,-7,5,3,-1]
+for num in list1:              #using membership operator to check if value exists in 'list1'& iterating each element in list.
+    if num>=0:                 #checking for positive number in list.
+        print(num,end=" ")
+print("\n")
+list2=[2,14,-45,3]
+for num in list2:              #using membership operator to check if value exists in 'list2'.
+    if num>=0:                 #checking for positive number in list.
+        print(num,end=" ")     
+        
